1d3cc97bb30be56e115d7d24711213b261713ee3
ayushchhanchar <chhancharayush007@gmail.com>
2024-09-30 14:07:31 +0530

    ques solves Ayush Chhanchar

--- program1.js
+++ program1.js
@@ -4,8 +4,28 @@
  */
 var isValid = function(s) {
     
+    let stack = [];
+    
+    const matchingPairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let char of s) {
+        if (matchingPairs[char]) {
+            stack.push(char);
+        } 
+        else {
+            const lastBracket = stack.pop();
+            if (matchingPairs[lastBracket] !== char) {
+                return false; 
+            }
+        }
+    }
+
+    return stack.length === 0;
+
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -3,6 +3,19 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanToIntMap = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    };
+
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (i < s.length - 1 && romanToIntMap[s[i]] < romanToIntMap[s[i + 1]]) {
+            total -= romanToIntMap[s[i]];
+        } else {
+            total += romanToIntMap[s[i]];
+        }
+    }
+    return total;
     
 };
 
